67ab3f5fb691a0f73c5361934a005a42e613a611
Ashish Bishnoi <ashishbishnoi18@gmail.com>
2018-04-27 00:43:41 +0530

    Add files via upload

---
+++ Wireless/airodump.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+$card = shell_exec('iw dev | awk \'$1=="Interface"{print $2}\' | sed -e \'2d\' ');
+$message = "Monitor mode not  enabled";
+$moncheck = shell_exec('iwconfig wlan1 | grep Monitor | sed -e "s/Mode:/ /" | awk \'{print $1}\' | cut -b 5');
+$attack = shell_exec("sudo timeout 18 airodump-ng wlan1 &> file.txt 2>&1");
+	if ($moncheck == 1) {
+		echo "<pre>$attack</pre>";
+	}
+	else {
+	echo "<pre>$message</pre>";
+	}
+?><br>
+</body>
+</html>

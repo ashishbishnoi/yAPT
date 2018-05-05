20cc73f6f065782c1e9430498f03c22363156eac
Ashish Bishnoi <ashishbishnoi18@gmail.com>
2018-05-05 09:55:00 +0530

    Update aireplay.php

--- Wireless/aireplay.php
+++ Wireless/aireplay.php
@@ -7,7 +7,7 @@ $npk = $_GET["npk"];
 $card = shell_exec('iw dev | awk \'$1=="Interface"{print $2}\' | sed -e \'2d\' ');
 $message = "Monitor mode not enabled";
 $moncheck = shell_exec('iwconfig wlan1 | grep Monitor | sed -e "s/Mode:/ /" | awk \'{print $1}\' | cut -b 5');
-$attack = shell_exec("sudo aireplay-ng -0 100 -a $bssid wlan1");
+$attack = shell_exec("sudo aireplay-ng -0 $npk -a $bssid wlan1");
 	if ($moncheck == 1) {
 		echo "<pre>$attack</pre>";
 	}
